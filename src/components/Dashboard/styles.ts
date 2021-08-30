197505658853e4b338064d778602a7b33dcf731a
hastalamuerteg <gabriel.vicent@hotmail.com>
2021-08-30 10:10:32 -0300

    init dashboard

---
+++ src/components/Dashboard/styles.ts
@@ -0,0 +1,7 @@
+import styled from "styled-components";
+
+export const Container = styled.main`
+  max-width: 80%;
+  margin: 0 auto;
+  padding: 2rem 1rem;
+`;
---
+++ src/components/Header/styles.ts
@@ -0,0 +1,28 @@
+import styled from "styled-components";
+
+export const Container = styled.header`
+  background: var(--blue);
+`;
+
+export const Content = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 2rem 1rem 10rem;
+  max-width: 80%;
+  margin: 0 auto;
+
+  button {
+    font-size: 1rem;
+    background: var(--blue-light);
+    padding: 0.8rem 2rem;
+    border: 0;
+    color: #fff;
+    border-radius: 0.2rem;
+    transition: filter 0.2s;
+
+    &:hover {
+      filter: brightness(0.9);
+    }
+  }
+`;
